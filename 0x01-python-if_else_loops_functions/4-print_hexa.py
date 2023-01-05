3727c763fad317c5edae0b8f4a60bdad8f7b1968
ifiokonko@gmail.com <ifiokonko@gmail.com>
2023-01-05 08:23:14 -0800

    task 4 corrected

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') and i != ord('e'):
-        print('{:c}'.format(i), end='')
+for i in range(0, 99):
+    print('{:d} = 0x{:x}'.format(i, i))
