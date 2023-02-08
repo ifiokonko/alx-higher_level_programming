e38cf3537bcdad15af84dd100b3109c3f8d641eb
ifiokonko@gmail.com <ifiokonko@gmail.com>
2023-02-07 20:47:50 -0800

    bug fix on task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
-
 #!/usr/bin/python3
-"""Module for Lookup function"""
+"""
+Program that Lookup all attributes and methods of an object
+"""
 
 
 def lookup(obj):
-    """ function that returns the list of available
-        attributes and methods of an object"""
-    return list(dir(obj))
+    """function that returns the list of available attributes
+    and methods of an object """
+    return (dir(obj))
